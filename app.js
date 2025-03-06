46071d8e6fddfea05c09f81f12e38a70cdd5e7ce
Stephan02-hub <braghesesteven@gmail.com>
2025-03-06 21:54:40 +0100

    add:feat

--- app.js
+++ app.js
@@ -1,63 +1,24 @@
-const express = require('express');
-const dolci = require('./data/dolci');
-const app = express()
-const port = 3000
-app.use(express.static('public'))
-const postsRouter = require("./routers/postRouter")
-app.use("/posts", postsRouter);
-const controller = require("../controllers/controlloCibo");
-const errorsHandler = require("./middlewares/errorsHandler")
+const express = require("express");
+const moviesRouter = require("./routers/movies");
+const errorsHandler = require("./middlewares/errorsHandler");
+const routeNotFound = require("./middlewares/routeNotFound");
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-  res.send('Server del mio blog')
-})
+const app = express();
+const { PORT, FE_URL } = process.env;
 
-// Middleware 404
-app.use(notFound);
-// Middleware 500
-app.use(errorsHandler);
-
-
-// index
-router.get('/dolci', (req, res) => {
-    res.json(postsDolci)
-})
-
-// show
-router.get('/dolci/:id', (req, res)=> {
-    if (isNaN(req.params.id)) {
-        return res.sendStatus(400);
-      }
-    
-      const posts = postsDolci.find((elm) => elm.id == req.params.id);
-    
-      if (posts) {
-        return res.sendStatus(404);
-      }
-
-      res.json(dolci);
-})
-
-// create
-router.post("/dolci", (req, res) => {
-    res.send("Creazione nuovo post");
-  });
-
-// update
-router.put("/dolci/:id", (req, res) => {
-    res.send(`Modifica integrale del post: ${req.params.id}`);
-  });
-
-// modify
-router.patch("/dolci/:id", (req, res) => {
-    res.send(`Modifica parziale del post: ${req.params.id}`);
-  });
-
-//delete
-router.delete("/dolci/:id", (req, res) => {
-    res.send(`Eliminazione del post: ${req.params.id}`);
-  });
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.use(express.json());
+app.use(
+  cors({
+    origin: FE_URL,
   })
+);
+app.use(express.static("public"));
+app.use("/movies", moviesRouter);
+
+app.use(errorsHandler);
+app.use(routeNotFound);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
---
+++ controllers/moviesControllers.js
@@ -0,0 +1,134 @@
+const connection = require("../data/db");
+
+//Index
+const index = (req, res) => {
+  const sql = `
+    SELECT movies.*,  ROUND(AVG(reviews.vote)) as media_vote
+    FROM movies
+    LEFT JOIN reviews ON movies.id = reviews.movie_id
+    GROUP BY movies.id `;
+
+  connection.query(sql, (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        error: "Query Error",
+        message: `Database query failed: ${sql}`,
+      });
+    }
+
+    const movies = results.map((movie) => {
+      movie.image = `${process.env.BE_URL}/movies_cover/${movie.image}`;
+      return movie;
+    });
+    res.json(movies);
+  });
+};
+
+const show = (req, res) => {
+  const { id } = req.params;
+
+  //   const moviesSql = `
+  //     SELECT *
+  //     FROM movies
+  //     WHERE id = ?`;
+  const moviesSql = `
+    SELECT movies.*,  ROUND(AVG(reviews.vote)) as media_vote
+    FROM movies
+    LEFT JOIN reviews ON movies.id = reviews.movie_id
+    WHERE movies.id= ?
+    GROUP BY movies.id`;
+
+  connection.query(moviesSql, [id], (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        error: "Query Error",
+        message: `Database query failed: ${moviesSql}`,
+      });
+    }
+    const movie = results[0];
+
+    if (!movie) {
+      return res.status(404).json({
+        error: "Not found",
+        message: "Movie not found",
+      });
+    }
+
+    movie.image = `${process.env.BE_URL}/movies_cover/${movie.image}`;
+
+    const reviewSql = `
+    SELECT *
+    FROM reviews
+    WHERE movie_id = ?`;
+
+    connection.query(reviewSql, [id], (err, results) => {
+      if (err) {
+        return res.status(500).json({
+          error: "Query Error",
+          message: `Database query failed: ${reviewSql}`,
+        });
+      }
+      movie.review = results;
+      res.json(movie);
+    });
+  });
+};
+
+const storeReview = (req, res) => {
+  const { id } = req.params;
+  const { name, text, vote } = req.body;
+
+  const sql =
+    "INSERT INTO reviews (movie_id, name, text, vote) VALUES (?, ?, ?, ?)";
+
+  connection.query(sql, [id, name, text, vote], (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        error: "Query Error",
+        message: `Database query failed: ${sql}`,
+      });
+    }
+    res.status(201).json({ id: results.insertId });
+  });
+};
+
+//store
+const store = (req, res) => {
+  const image = req.file.filename;
+  const { title, genre, director, abstract, release_year } = req.body;
+
+  const sql =
+    "INSERT INTO movies (title, genre, director, abstract, release_year, image) VALUES (?, ?, ?, ?, ?, ?)";
+  connection.query(
+    sql,
+    [title, genre, director, abstract, release_year, image],
+    (err, results) => {
+      if (err) {
+        return res.status(500).json({
+          error: "Query Error",
+          message: `Database query failed: ${sql}`,
+        });
+      }
+      res.status(201).json({ id: results.insertId });
+    }
+  );
+};
+
+//destroy
+const destroy = (req, res) => {
+  const { id } = req.params;
+
+  const sql = `DELETE FROM movies WHERE id= ?`;
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        error: "Failed to delete movie",
+        message: `Failed to delete movie: ${id}`,
+      });
+    }
+
+    res.sendStatus(204);
+  });
+};
+module.exports = { index, show, storeReview, store, destroy };
